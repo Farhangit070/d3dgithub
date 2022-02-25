4e2112490e6f869a20646e4e5abfc81b7ce17cde
farhan ali hamdani <farhanhamdanidev@gmail.com>
2022-02-25 23:29:00 +0500

    remove firebase

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from '../about/about.component';
+
+
+
+const routes: Routes = [
+  {
+  path: './about/about.component', component: AboutComponent,
+  },
+  ];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+    ]
+})
+export class AppRoutingModule { }
